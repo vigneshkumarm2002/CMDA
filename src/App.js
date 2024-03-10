c41937eb4b9879160564b5ef8d5ca27aa184506b
vigneshkumarm2002 <mvigneshkumarbvn@gmail.com>
2024-03-10 13:10:19 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./Components/header";
 import Sidebar from "./Components/sidebar";
