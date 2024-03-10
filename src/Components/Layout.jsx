a6d2b1941b7ce2a3bdfe685c4e107cf785e20eef
vigneshkumarm2002 <mvigneshkumarbvn@gmail.com>
2024-03-10 15:23:35 +0530

    works

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Sidebar from './sidebar';
 import FileDetails from './homePages';
 import Checkout from './checkout';
 import PaymentPage from './paymentPage';
@@ -10,31 +12,30 @@ import ZonewiseReport from './zonewiseReport';
 import HeaderwiseReport from './headerwiseReport';
 import GatewaywiseReport from './gatewaywiseReport';
 
-import {BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Sidebar from './sidebar';
+const Layout = ({ sidebarOpen, setSidebarOpen }) => {
+  const location = useLocation();
+  const hideSidebarRoutes = ['/payment', '/checkout', '/payment-page', '/payment-confirm'];
 
-const Layout = ({sidebarOpen,setSidebarOpen}) => {
+  // Function to check if the current location matches any of the routes that should hide the sidebar
+  const shouldHideSidebar = () => {
+    return hideSidebarRoutes.includes(location.pathname);
+  };
 
-  
-    return (
+  return (
+    <div className="flex lg:gap-4">
+      {!shouldHideSidebar() && <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />}
+      <div className={` ${shouldHideSidebar() ? 'pt-[78px]' : 'w-full overflow-hidden lg:pl-[260px] pt-[78px] py-4 px-4'}`}>
+        <Routes>
+          <Route path="/transaction-report" element={<TransactionReport />} />
+          <Route path="/user-master" element={<UserMaster />} />
+          <Route path="/user-access-rights" element={<UserAccessRights />} />
+          <Route path="/localbody-report" element={<ZonewiseReport />} />
+          <Route path="/headerwise-report" element={<HeaderwiseReport />} />
+          <Route path="/gatewaywise-report" element={<GatewaywiseReport />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
-          <div className="flex lg:gap-4 ">
-      
-              <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-         
-            <div className="w-full overflow-hidden lg:pl-[260px] pt-[78px] py-4 px-4">
-              <Routes>
-            
-                <Route path="/transaction-report" element={<TransactionReport />} />
-                <Route path="/user-master" element={<UserMaster />} />
-                <Route path="/user-access-rights" element={<UserAccessRights />} />
-                <Route path="/localbody-report" element={<ZonewiseReport />} />
-                <Route path="/headerwise-report" element={<HeaderwiseReport />} />
-                <Route path="/gatewaywise-report" element={<GatewaywiseReport />} />
-              </Routes>
-            </div>
-          </div>
-  )
-}
-
-export default Layout
+export default Layout;
